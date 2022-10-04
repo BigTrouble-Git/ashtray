30d5b40c6d6863cb6821806270c20c19e01eb793
BigTrouble-Git <stephansnigur@gmail.com>
2022-10-04 18:32:34 +0300

    finishe3.1

--- Lab-3.1/Source.cpp
+++ Lab-3.1/Source.cpp
@@ -14,20 +14,19 @@ using namespace std;
 int main() {
 	double y;
 	double x; 	cin >> x;
-	double z;
-	double m;
+	
 	if (x < 4) {
-		y = 5 * pow(x, 8) + pow(x, 6) - pow(x, 2) + 3;
+		y = pow(x,3) + 2 * 5 * pow(x, 8) + pow(x, 6) - pow(x, 2) + 3;
 		cout << y;// << "y";
 	}
 
 	if (4 <= x && x < 7) {
-		z = pi / 2 - atan((x + 3) / 2) + 7 * x;
-		cout << z;// << "z";
+		y = pow(x, 3) + 2 * pi / 2 - atan((x + 3) / 2) + 7 * x;
+		cout << y;// << "z";
 	}
 
 	if (x >= 7) {
-		m = log10(2 * x + pow(e, 5 * x + 5));
-		cout << m;// << "m";
+		y = pow(x, 3) + 2 * log10(2 * x + pow(e, 5 * x + 5));
+		cout << y;// << "m";
 	}
 }
