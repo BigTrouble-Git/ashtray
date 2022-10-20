f70f0711b31dbd24fe20a1bcf82e63c629335627
BigTrouble-Git <stephansnigur@gmail.com>
2022-10-20 23:04:44 +0300

    lab3.1

--- Lab-3.1/Source.cpp
+++ Lab-3.1/Source.cpp
@@ -12,41 +12,46 @@
 using namespace std;
 
 int main() {
-	double y;
+	double y, y1, y2;
 	double x; 
+	double A;
+	double B;
 	cout << " x = ";	cin >> x;
+	A = pow(x, 3) + 2;
 	//скорочений варіант
 	if (x < 4) {
-		y = pow(x, 3) + 2 + 5 * pow(x, 8) + pow(x, 6) - pow(x, 2) + 3;
+		B =  5 * pow(x, 8) + pow(x, 6) - pow(x, 2) + 3;
 		
 	}
 
 	if (4 <= x && x < 7) {
-		y = pow(x, 3) + 2 + (4 * atan(1.0)) / 2 - atan(abs((x + 3) / 2)) + 7 * x;
+		B =  (4 * atan(1.0)) / 2 - atan(abs((x + 3) / 2)) + 7 * x;
 		
 	}
 
 	if (x >= 7) {
-		y = pow(x, 3) + 2 + log10(2 * x + exp(5 * x + 5));
+		B = log10(2 * x + exp(5 * x + 5));
 		
 	
 	}
-	cout << " y1 = " << y << endl;
+	y1 = A + B;
+	cout << " y1 = " << y1 << endl;
 
 	//повний варіант 
 		if (x < 4) {
-		y = pow(x, 3) + 2 + 5 * pow(x, 8) + pow(x, 6) - pow(x, 2) + 3;
+			B = 5 * pow(x, 8) + pow(x, 6) - pow(x, 2) + 3;
 	
 	}
 	else if (4 <= x && x < 7) {
-		y = pow(x, 3) + 2 + (4 * atan(1.0)) / 2 - atan(abs((x + 3) / 2)) + 7 * x;
+			B = (4 * atan(1.0)) / 2 - atan(abs((x + 3) / 2)) + 7 * x;
 	
 	}
 	else {
-		y = pow(x, 3) + 2 + log10(2 * x + exp(5 * x + 5));
+			B = log10(2 * x + exp(5 * x + 5));
 		
-	}
-		cout << " y2 = " << y << endl;
+	} 
+		y2 = A + B;
+		cout << " y2 = " << y2 << endl;
 		cin.get();
 		return 0;
 }
